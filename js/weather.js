14c9be4d9f22fa4eaf17b2e079ea736a6d0c8335
wook888 <guddnr888@gmail.com>
2023-06-18 04:58:16 +0900

    API gitignore 등록

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,11 @@
+import { config } from "./apikey.js";
+
 const WEATHER_API = config.apikey;
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
   console.log(url);
   fetch(url)
     .then((response) => response.json())
@@ -15,6 +17,7 @@ function onGeoOk(position) {
       weather.innerText = `${data.weather[0].main} / ${data.main.temp};`;
     });
 }
+
 function onGeoError() {
   alert("Can't find you. No weather for you.");
 }
